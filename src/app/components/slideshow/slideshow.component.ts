0c0ca0d02f2b67bca35e447c778640b983cad8fe
mabvmex <mabvmex@gmail.com>
2020-12-18 16:20:28 -0600

    Swiper - Slideshow de peliculas

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,17 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../interfaces/cartelera-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.css']
+  styleUrls: ['./slideshow.component.css'],
 })
-export class SlideshowComponent implements OnInit {
-
+export class SlideshowComponent implements OnInit, AfterViewInit {
   @Input() movies: Movie[];
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+
+      // // If we need pagination
+      // pagination: {
+      //   el: '.swiper-pagination',
+      // },
+
+      // // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+
+      // // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
+  }
 
   ngOnInit(): void {
     console.log(this.movies);
   }
-
 }
