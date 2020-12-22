7db0ff9045656031e2a184edee60d1443f070036
mabvmex <mabvmex@gmail.com>
2020-12-21 23:33:44 -0600

    InfiniteScroll de peliculas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Movie } from '../../interfaces/cartelera-response';
 
@@ -10,6 +10,20 @@ import { Movie } from '../../interfaces/cartelera-response';
 export class HomeComponent implements OnInit {
 
   public movies: Movie[] = [];
+  public moviesSlideShow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop ) + 1300;
+    const max =  (document.documentElement.scrollHeight || document.body.scrollHeight);
+
+    if (pos > max) {
+      this.peliculasService.getCartelera().subscribe(resp => {
+        this.movies.push(...resp.results);
+      });
+      console.log('Llamar servicio');
+    }
+  }
 
   constructor( private peliculasService: PeliculasService ) {
   }
@@ -19,8 +33,17 @@ export class HomeComponent implements OnInit {
     this.peliculasService.getCartelera()
     .subscribe(resp => {
       this.movies = resp.results;
-    });
+      this.moviesSlideShow = resp.results;
+    })
 
   }
 
 }
+
+
+/*
+
+Decorador (de una función) @Hostlistener: Escucha un evento propio del host: Escucho el window --> Scroll
+TAP: Dispara un efecto secundario. Se ejecuta el código cada vez el observable emite un valor. EL tab no modifica ni altera nada (es un pipe)
+
+*/
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -2,18 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PeliculasService {
 
-  constructor( private http: HttpClient ) { }
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
 
+  constructor(private http: HttpClient) {}
 
-  getCartelera(): Observable<CarteleraResponse> {
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=8a8db5f649c0efd39b1d197bb24d998b&language=es-ES&page=1')
+  get params() {
+    return {
+      api_key: '8a8db5f649c0efd39b1d197bb24d998b',
+      language: 'es-ES',
+      page: this.carteleraPage.toString(),
+    };
   }
 
+  getCartelera(): Observable<CarteleraResponse> {
 
+    return this.http.get<CarteleraResponse>( `${this.baseUrl}/movie/now_playing`, {
+      params: this.params
+    }).pipe(
+      tap( () => {
+        this.carteleraPage += 1;
+      })
+    );
+
+  }
 }
